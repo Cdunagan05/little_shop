e73e9a38cfeb94362f627f8798deb858ad17f500
Brian Heim <bheim67@gmail.com>
2016-09-13 14:11:19 -0600

    add item model and seed data, closes #3

--- spec/features/visitor_can_view_items_spec.rb
+++ spec/features/visitor_can_view_items_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 RSpec.feature "Visitor can visit Item index" do
-  let(:item) do
-    Item.create(title: "Broncos Jersey", description: "This is a sweet Home Broncs Jersey yo", price: 100.00)
-  end
+  # let(:item) do
+  #   Item.create(title: "Broncos Jersey", description: "This is a sweet Home Broncs Jersey yo", price: 100.00)
+  # end
   scenario "they can visit item index and see all existing items" do
+    Item.create(title: "Broncos Jersey", description: "This is a sweet Home Broncs Jersey yo", price: 100.00)
     # As a visitor
     # When I visit "/items"
     visit '/items'
