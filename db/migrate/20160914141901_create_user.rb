2cdf55d55898fa6d56851ff6c4693d01f5e7af67
Brian Heim <bheim67@gmail.com>
2016-09-14 11:37:58 -0600

    wip visitor create account test

---
+++ db/migrate/20160914141901_create_user.rb
@@ -0,0 +1,10 @@
+class CreateUser < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password
+
+      t.timestamps null: false
+    end
+  end
+end
