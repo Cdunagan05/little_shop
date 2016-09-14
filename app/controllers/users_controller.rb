039ccc8befe66f2c91bcdeae6354f4ffe6e8a606
Bryan Goss <bcgoss@gmail.com>
2016-09-14 13:18:07 -0600

    Fixed hound complaints

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   def new
     @user = User.new
   end
@@ -19,6 +18,7 @@ class UsersController < ApplicationController
   end
 
   private
+
   def user_params
     params.require(:user).permit(:username, :password, :password_confirmation)
   end
