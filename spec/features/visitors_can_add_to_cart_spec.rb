02ab383e8ca087ae604e8381c3e9e9f5cf9753a2
Bryan Goss <bcgoss@gmail.com>
2016-09-13 21:04:09 -0600

    Fixed Hound

--- spec/features/visitors_can_add_to_cart_spec.rb
+++ spec/features/visitors_can_add_to_cart_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.feature 'Visitors can add to cart' do
   scenario 'they can add an item to a cart' do
   #  As a visitor
- # When I visit any page with an item on it
- # I should see a link or button for 'Add to Cart'
- # When I click 'Add to cart' for that item
- # And I click a link or button to view cart
- # And my current path should be '/cart'
- # And I should see a small image, title, description and price for the item I just added
- # And there should be a 'total' price for the cart that should be the sum of all items in the cart
+  # When I visit any page with an item on it
+  # I should see a link or button for 'Add to Cart'
+  # When I click 'Add to cart' for that item
+  # And I click a link or button to view cart
+  # And my current path should be '/cart'
+  # And I should see a small image, title, description and price for the item I just added
+  # And there should be a 'total' price for the cart that should be the sum of all items in the cart
 
   create(:item)
 
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
   it 'can be initialized with contents' do
-    cart = Cart.new({'1' => 1})
+    cart = Cart.new('1' => 1)
 
-    expect(cart.contents).to eq( {'1' => 1} )
+    expect(cart.contents).to eq('1' => 1)
   end
 
 
