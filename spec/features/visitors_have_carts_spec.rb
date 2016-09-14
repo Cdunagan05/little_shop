261716c6dd909752256fed4666f199385d1aa12c
Bryan Goss <bcgoss@gmail.com>
2016-09-14 14:46:51 -0600

    Refactored test to eliminate un-needed data

--- spec/features/visitors_have_carts_spec.rb
+++ spec/features/visitors_have_carts_spec.rb
@@ -3,14 +3,13 @@ require 'rails_helper'
 RSpec.feature 'Visitors have carts' do
   scenario 'a visitor adds items and views the cart' do
     # As a visitor when I have items in my cart
-    item_1, item_2 = create_list(:item, 2)
+    item_1 = create :item
     visit '/items'
-    click_button 'Add to Cart', match: :first
+    click_button 'Add to Cart'
     # session[:cart] = create_a_cart_with_items([item_1, item_2])
     # And when I visit '/cart'
     visit '/cart'
     # I should not see an option to 'Checkout'
-    save_and_open_page
     expect(page).to_not have_button 'Checkout'
     # I should see an option to 'Login or Create Account to Checkout'
     expect(page).to have_link 'Login or Create Account to Checkout'
@@ -21,5 +20,6 @@ RSpec.feature 'Visitors have carts' do
     visit '/cart'
     # Then I should see all of the data that was there when I was not logged in
     expect(page).to have_button 'Checkout'
+    expect(page).to have_content item_1.title
   end
 end
