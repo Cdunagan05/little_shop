f75c8ec55402514ba648ee723f2a2a5f236dd6e0
Brian Heim <bheim67@gmail.com>
2016-09-17 11:55:11 -0600

    add model test for order

---
+++ spec/models/order_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Order, type: :model do
+  context 'associations' do
+    it {should belong_to(:user)}
+    it {should have_many(:items).through(:order_items)}
+  end
+end
