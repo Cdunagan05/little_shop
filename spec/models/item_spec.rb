02c2e9a98b8a8764bff12de1956135929df0411f
Brian Heim <bheim67@gmail.com>
2016-09-17 13:06:09 -0600

    Update item_spec.rb

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   context 'associations' do
-    it {should have_many(:categories).through(:item_categories)}
-    it {should have_many(:orders).through(:order_items)}
+    it { should have_many(:categories).through(:item_categories) }
+    it { should have_many(:orders).through(:order_items) }
   end
 end
