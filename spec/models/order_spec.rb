dd732ab8739d649af129c3615242beeae3a08dc2
Brian Heim <bheim67@gmail.com>
2016-09-17 13:06:36 -0600

    Update order_spec.rb

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Order, type: :model do
   context 'associations' do
-    it {should belong_to(:user)}
-    it {should have_many(:items).through(:order_items)}
+    it { should belong_to(:user) }
+    it { should have_many(:items).through(:order_items) }
   end
 
-  it {should define_enum_for(:status)}
+  it { should define_enum_for(:status) }
 end
