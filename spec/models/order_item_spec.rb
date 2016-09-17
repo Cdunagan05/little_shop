d2552eedc1ba73e0982f212409913bc1d38bcf63
Brian Heim <bheim67@gmail.com>
2016-09-17 13:06:22 -0600

    Update order_item_spec.rb

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe OrderItem, type: :model do
   context 'associations' do
-    it {should belong_to(:order)}
-    it {should belong_to(:item)}
+    it { should belong_to(:order) }
+    it { should belong_to(:item) }
   end
 end
