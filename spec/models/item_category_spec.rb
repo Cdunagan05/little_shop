f45e56b9c805455c5fc0a676a3fda446f7d9c39f
Brian Heim <bheim67@gmail.com>
2016-09-17 11:43:05 -0600

    add model test for item_category

---
+++ spec/models/item_category_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe ItemCategory, type: :model do
+  context 'validations' do
+    it {should belong_to(:item) }
+    it {should belong_to(:category) }  
+  end
+end
