6bd5f97c8bd88170b0f920604680278a9ae178c6
Brian Heim <bheim67@gmail.com>
2016-09-17 13:05:55 -0600

    Update item_category_spec.rb

--- spec/models/item_category_spec.rb
+++ spec/models/item_category_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe ItemCategory, type: :model do
   context 'associations' do
-    it {should belong_to(:item) }
-    it {should belong_to(:category) }
+    it { should belong_to(:item) }
+    it { should belong_to(:category) }
   end
 end
