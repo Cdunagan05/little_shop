b898d9ac90c56499f1ac26910c10b5eaf666a411
Bryan Goss <bcgoss@gmail.com>
2016-09-18 10:37:06 -0600

    Add tests for order.set_item and order.total

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -7,4 +7,18 @@ RSpec.describe Order, type: :model do
   end
 
   it { should define_enum_for(:status) }
+
+  it "creates order items for each item on the order" do
+    item = create :item
+    order = create :order_for_user
+    expect{ order.set_items(item => 1) }.to change{ OrderItem.all.count }.from(0).to(1)
+  end
+
+  it "totals up subtotals from order items" do
+    item = create :item
+    order = create :order_for_user
+    order.set_items(item => 2)
+
+    expect(order.total).to eq(200)
+  end
 end
