bbfe26c6a024ba7a62a3bb76072b9c22dcf3d434
Bryan Goss <bcgoss@gmail.com>
2016-09-18 18:15:24 -0600

    add route for checkout button

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,14 @@
 class OrdersController < ApplicationController
+  def create
+    order = current_user.orders.new
+    order.subtotal_order_items(@cart.item_quantities)
+    if order.save
+      flash[:success] = 'Order was successfully placed'
+      redirect_to :orders_path
+    else
+      redirect_back(fallback_location: cart_path)
+    end
+  end
   def index
     if current_user
       @orders = current_user.orders
