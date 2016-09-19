4d1b5ab036ace399c917230d95258da5ef321adb
Chase Dunagan <cdunagan@utexas.edu>
2016-09-19 11:17:14 -0600

    add item model validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,8 @@ class Item < ApplicationRecord
 
   has_many :order_items
   has_many :orders, through: :order_items
+
+  validates :title, presence: true, uniqueness: true
+  validates :price, presence: true
+  validates :image, presence: true
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe Item, type: :model do
     it { should have_many(:categories).through(:item_categories) }
     it { should have_many(:orders).through(:order_items) }
   end
+
+  context 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:price) }
+    it { should validate_presence_of(:image) }
+    it { should validate_uniqueness_of(:title) }
+  end
 end
