695387f16745199ba14ea5aeb15af107beaed789
Bryan Goss <bcgoss@gmail.com>
2016-09-20 10:26:44 -0600

    Format view for a single order to contain all required information
    correct typo in order lines
    
    Update dates to use long formatting
    
    Fixed hound
    
    Fixed hound

--- spec/features/admin_views_an_individual_order_spec.rb
+++ spec/features/admin_views_an_individual_order_spec.rb
@@ -2,37 +2,38 @@ require 'rails_helper'
 
 RSpec.feature 'Admin can view an individual order' do
   scenario 'Login as admin and view an order' do
-#     As an authenticated Admin,
     admin = create :user, role: 1
     login_user(admin)
+
     billy = create :user, name: 'Billybob Thorton', address: '123 Fake St'
     order = build :order, user: billy
+
     item1 = create :item
     item2 = create :item
-    order.subtotal_order_items({item1 => 2, item2 => 1})
+
+    order.subtotal_order_items(item1 => 2, item2 => 1)
     order.save
- # when I visit an individual order page
+
     visit '/orders/8'
-# Then I can see the order's date and time.
-    expect(page).to have_content(order.created_at)
-# And I can see the purchaser's full name and address.
+
+    expect(page).to have_content(order.created_at.to_formatted_s(:long))
     expect(page).to have_content('Billybob Thorton')
     expect(page).to have_content('123 Fake St')
-# And I can see, for each item on the order:
-# - The item's name, which is linked to the item page.
-# - Quantity in this order.
-# - Price
-# - Line item subtotal.
-save_and_open_page
     expect(page).to have_link('Jersey 7')
-    
-    expect(page).to have_content('Quantity 2')
-    expect(page).to have_content(100.0)
-    expect(page).to have_content(200.0)
-# And I can see the total for the order.
-    expect(page).to have_content(300.0)
-# And I can see the status for the order.
-    expect(page).to have_content('Status: Ordered')
 
+    within('tr:nth-child(2)') do
+      within('td:nth-child(2)') do
+        expect(page).to have_content('2')
+      end
+      within('td:nth-child(3)') do
+        expect(page).to have_content(100.0)
+      end
+      within('td:nth-child(4)') do
+        expect(page).to have_content(200.0)
+      end
+    end
+
+    expect(page).to have_content('Total: $300.0')
+    expect(page).to have_content('Status: Ordered')
   end
 end
